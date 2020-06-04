8676cdba7b9f3cfed7e18c906092dbe9c7ff5b6e
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-04 15:50:56 -0500

    item file fixed

--- src/item.py
+++ src/item.py
@@ -1,26 +1,16 @@
-from item import Item
+class Item :
+     def __init__ (self, name, description):
+         self.name = name
+         self.description = description
 
-class Room:
-    def __init__(self, name, description, n_to, s_to, e_to, w_to):
-        self.name = name
-        self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
-        self.items = []
+     def on_take (self):
+         print(F"You picked up the {self.name}.")
 
-    def get_items (self):
-        return self.items
+     def on_drop (self):
+         print(F"You have dropped the {self.name}.")
 
-    def add_item (self, item):
-        return self.items.append(item)
+     def __repr__ (self):
+         return (F"Name: {self.name}, Description: {self.description}")
 
-    def take_item (self, item):
-        return self.items.remove(item)
-
-    def __repr__ (self):
-        return (F"Name: {self.name}, Description: {self.description}, Items: {str(self.items)}")
-
-    def __str__ (self):
-        return (F"Name: {self.name}, Description: {self.description}, Items: {str(self.items)}") 
+     def __str__ (self):
+         return (F"Name: {self.name}, Description: {self.description}")
